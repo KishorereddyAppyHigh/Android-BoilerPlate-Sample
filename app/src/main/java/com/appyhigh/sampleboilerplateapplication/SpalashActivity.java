b3be9b3f276fe0372eee087aec7e6c81537183c1
KishorereddyAppyHigh <kishore@appyhigh.com>
2020-07-09 08:17:48 +0530

    [kishorereddy] Add. added SampleBoilerApplication

---
+++ app/src/main/java/com/appyhigh/sampleboilerplateapplication/SpalashActivity.java
@@ -0,0 +1,14 @@
+package com.appyhigh.sampleboilerplateapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpalashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalash);
+    }
+}
