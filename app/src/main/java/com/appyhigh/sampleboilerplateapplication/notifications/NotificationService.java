93cb7e73bfa3f1b2199514b240a4d0a6acd83d6f
KishorereddyAppyHigh <kishore@appyhigh.com>
2020-07-10 11:05:27 +0530

    [kishorereddy] Refactor. added pendingintent to NotificationService.java

--- app/src/main/java/com/appyhigh/sampleboilerplateapplication/notifications/NotificationService.java
+++ app/src/main/java/com/appyhigh/sampleboilerplateapplication/notifications/NotificationService.java
@@ -1,11 +1,15 @@
 package com.appyhigh.sampleboilerplateapplication.notifications;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.appyhigh.sampleboilerplateapplication.MainActivity;
 import com.appyhigh.sampleboilerplateapplication.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -21,10 +25,14 @@ public class NotificationService extends FirebaseMessagingService {
     }
 
     private void showNotification(String title, String body) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
                 .setContentTitle(title)
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
                 .setContentText(body);
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
         notificationManagerCompat.notify(id,builder.build());
