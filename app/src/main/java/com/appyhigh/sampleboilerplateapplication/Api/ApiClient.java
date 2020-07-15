758de7b5fb27c6f28b4d01be4bc9f9126e0b1c8d
KishorereddyAppyHigh <kishore@appyhigh.com>
2020-07-15 16:16:23 +0530

    [kishorereddy] Add. added news Api classes

---
+++ app/src/main/java/com/appyhigh/sampleboilerplateapplication/Api/ApiClient.java
@@ -0,0 +1,73 @@
+package com.appyhigh.sampleboilerplateapplication.Api;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    public static final String BASE_URL = "http://newsapi.org/v2/";
+    public static Retrofit retrofit;
+
+    public static Retrofit getApiClient(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static OkHttpClient.Builder getUnsafeOkHttpClient() {
+        try {
+            final TrustManager[] trustManagers = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                    }
+            };
+            //install All trusting trust managers
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null , trustManagers ,new java.security.SecureRandom());
+
+            //create an ssl socket factory with our all trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustManagers[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+            return  builder;
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ app/src/main/java/com/appyhigh/sampleboilerplateapplication/Api/ApiInterface.java
@@ -0,0 +1,16 @@
+package com.appyhigh.sampleboilerplateapplication.Api;
+
+import com.appyhigh.sampleboilerplateapplication.Model.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("top-headlines")
+    Call<News> getNews(
+        @Query("country") String country,
+        @Query("apiKey") String apiKey
+    );
+}
+
