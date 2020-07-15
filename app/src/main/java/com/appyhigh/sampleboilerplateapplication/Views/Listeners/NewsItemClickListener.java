523a7ebb1ebfdcecf1f2de5f6957f76185473df4
KishorereddyAppyHigh <kishore@appyhigh.com>
2020-07-15 16:18:54 +0530

    [kishorereddy] Add. added listeners

---
+++ app/src/main/java/com/appyhigh/sampleboilerplateapplication/Views/Listeners/NewsItemClickListener.java
@@ -0,0 +1,8 @@
+package com.appyhigh.sampleboilerplateapplication.Views.Listeners;
+
+import android.view.View;
+
+
+public interface NewsItemClickListener {
+    void onItemClick(View view , int position);
+}
