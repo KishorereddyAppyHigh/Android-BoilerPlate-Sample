aca058e8654d8f2a2e49b37528f71a841515a36c
KishorereddyAppyHigh <kishore@appyhigh.com>
2020-07-16 09:18:24 +0530

    [kishorereddy] Refactor. refactored ApiClient.java

--- app/src/main/java/com/appyhigh/sampleboilerplateapplication/Api/ApiClient.java
+++ app/src/main/java/com/appyhigh/sampleboilerplateapplication/Api/ApiClient.java
@@ -18,12 +18,13 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "http://newsapi.org/v2/";
+    public static final String BASE_URL = "https://newsapi.org/v2/";
     public static Retrofit retrofit;
 
     public static Retrofit getApiClient(){
         if (retrofit == null){
             retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                    .client(getUnsafeOkHttpClient().build())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
